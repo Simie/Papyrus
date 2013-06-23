d66e7e8f02accfb1888f49edb860499115e534e4
Simon Moles <simon.moles@gmail.com>
2013-06-23 22:41:55 +0100

    Add RecordTests

---
+++ Papyrus.Tests/RecordTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Papyrus.Tests
+{
+	[TestClass]
+	public class RecordTests
+	{
+
+		[TestMethod]
+		public void TestFreeze()
+		{
+
+			var record = new TestRecord();
+
+			record.IsFrozen = false;
+
+			try {
+				record.SetProperty(() => record.TestBoolean, true);
+			} catch (InvalidOperationException) {
+				Assert.Fail("Threw exception when record was not frozen.");
+			}
+
+			record.IsFrozen = true;
+	
+			try {
+				record.SetProperty(() => record.TestBoolean, true);
+				Assert.Fail("Didn't throw exception when record was frozen.");
+			} catch (InvalidOperationException) {}
+
+		}
+
+	}
+}
