eb76b32ed03db265eac928168bd2a7d8623d6511
Simon Moles <simon.moles@gmail.com>
2013-06-24 22:45:13 +0100

    Added license header

--- Papyrus.Core/Freezable.cs
+++ Papyrus.Core/Freezable.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Copyright © 2013 Stompy Robot (http://www.stompyrobot.co.uk) (https://github.com/stompyrobot)
+ * 
+ * This program is licensed under the Microsoft Public License (Ms-PL). You should
+ * have received a copy of the license along with the source code. If not, an online copy
+ * of the license can be found at https://github.com/stompyrobot/Papyrus/wiki/License.
+ */
+using System;
 using System.Globalization;
 using System.Linq.Expressions;
 using System.Reflection;
--- Papyrus.Studio/Modules/Startup/Module.cs
+++ Papyrus.Studio/Modules/Startup/Module.cs
@@ -1,10 +1,11 @@
 ﻿/*
- * Copyright © 2012 Stompy Robot (http://www.stompyrobot.co.uk) (https://github.com/stompyrobot)
+ * Copyright © 2013 Stompy Robot (http://www.stompyrobot.co.uk) (https://github.com/stompyrobot)
  * 
- * This program is licensed under the Microsoft Permissive License (Ms-PL). You should
+ * This program is licensed under the Microsoft Public License (Ms-PL). You should
  * have received a copy of the license along with the source code. If not, an online copy
  * of the license can be found at https://github.com/stompyrobot/Papyrus/wiki/License.
  */
+
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.Composition;
