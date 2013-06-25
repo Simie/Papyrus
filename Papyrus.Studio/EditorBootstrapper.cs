663c58efac3db33f62d1a64c034ae0c9163a4a10
Simon Moles <simon.moles@gmail.com>
2013-06-25 23:05:13 +0100

    Added close strategy

---
+++ Papyrus.Studio/EditorBootstrapper.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Copyright © 2012 Stompy Robot (http://www.stompyrobot.co.uk) (https://github.com/stompyrobot)
+ * 
+ * This program is licensed under the Microsoft Permissive License (Ms-PL). You should
+ * have received a copy of the license along with the source code. If not, an online copy
+ * of the license can be found at https://github.com/stompyrobot/Papyrus/wiki/License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using Caliburn.Micro;
+using Gemini;
+using Gemini.Framework;
+using Gemini.Framework.Services;
+
+namespace Papyrus.Studio
+{
+	public class EditorBootstrapper : AppBootstrapper
+	{
+
+		public static readonly List<string> PapyrusModules = new List<string>();
+
+		public List<string> Modules { get { return PapyrusModules; } } 
+
+		protected override void OnStartup(object sender, System.Windows.StartupEventArgs e)
+		{
+			
+			base.OnStartup(sender, e);
+
+			var shell = IoC.Get<IShell>();
+
+			var conductor = shell as Conductor<IDocument>.Collection.OneActive;
+
+			if (conductor == null) {
+				throw new InvalidOperationException("Could not locate Conductor. Has Gemini changed implementation?");
+			}
+
+			conductor.CloseStrategy = new PapyrusCloseStrategy();
+
+		}
+
+	}
+}
---
+++ Papyrus.Studio/PapyrusCloseStrategy.cs
@@ -0,0 +1,94 @@
+﻿/*
+ * Copyright © 2012 Stompy Robot (http://www.stompyrobot.co.uk) (https://github.com/stompyrobot)
+ * 
+ * This program is licensed under the Microsoft Permissive License (Ms-PL). You should
+ * have received a copy of the license along with the source code. If not, an online copy
+ * of the license can be found at https://github.com/stompyrobot/Papyrus/wiki/License.
+ */
+using System;
+using System.Collections.Generic;
+using Caliburn.Micro;
+using Gemini.Framework;
+using Papyrus.Studio.Framework.Results;
+using Papyrus.Studio.Modules.PapyrusManager;
+using Gemini.Framework.Services;
+
+namespace Papyrus.Studio
+{
+
+	/// <summary>
+	/// Heck fest to query papyrus manager before closing
+	/// </summary>
+	class PapyrusCloseStrategy : ICloseStrategy<IDocument>
+	{
+
+		private readonly DefaultCloseStrategy<IDocument> _internalStrategy;
+
+		public PapyrusCloseStrategy()
+		{
+			_internalStrategy = new DefaultCloseStrategy<IDocument>(false);
+		}
+
+		IEnumerable<IResult> ReportError(Exception e)
+		{
+			yield return ShowExt.Exception(e);
+		} 
+
+		public void Execute(IEnumerable<IDocument> toClose, Action<bool, IEnumerable<IDocument>> callback)
+		{
+
+
+			var conductor = IoC.Get<IShell>() as Caliburn.Micro.Conductor<IDocument>.Collection.OneActive;
+
+			if (conductor == null) {
+				Coroutine.BeginExecute(ReportError(new InvalidOperationException("Could not locate Conductor. Has Gemini changed implementation?")).GetEnumerator());
+				return;
+			}
+
+			// If it's closing only a few items, pass it on to the default strategy
+			if (conductor.Items != toClose) {
+				_internalStrategy.Execute(toClose, callback);
+				return;
+			}
+
+			Framework.SaveUtil.BeginSaveOperation();
+
+			Exception error = null;
+
+			try {
+
+				// If it's a full shutdown, we're going to invervene
+				_internalStrategy.Execute(toClose, (editorsCanClose, screens) => {
+
+					Framework.SaveUtil.EndSaveOperation();
+
+					// If an editor has already cancelled shutdown, we don't need to stop it.
+					if (!editorsCanClose) {
+						callback(editorsCanClose, screens);
+						return;
+					}
+
+
+
+					var p = IoC.Get<IPapyrusManager>() as
+					        Modules.PapyrusManager.ViewModels.PapyrusManagerViewModel;
+
+					// Check if papyrus has a problem closing
+					p.CanClose(papyrusCanClose => callback(papyrusCanClose, screens));
+
+				});
+
+			} catch (Exception e) {
+				error = e;
+				Framework.SaveUtil.EndSaveOperation();
+			}
+
+			if (error != null) {
+				Coroutine.BeginExecute(ReportError(error).GetEnumerator());
+			}
+
+
+		}
+
+	}
+}
