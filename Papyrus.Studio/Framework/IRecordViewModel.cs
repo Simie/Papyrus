d887d8b3a7f05498314f7c20d4a7dfb9ddac52d0
Simon Moles <simon.moles@gmail.com>
2014-01-17 23:49:02 +0000

    Update license header

--- Papyrus.Studio/Framework/IRecordViewModel.cs
+++ Papyrus.Studio/Framework/IRecordViewModel.cs
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2013 Stompy Robot (http://www.stompyrobot.co.uk) (https://github.com/stompyrobot)
+ * Copyright © 2014 Stompy Robot (http://www.stompyrobot.co.uk) (https://github.com/stompyrobot)
  * 
  * This program is licensed under the Microsoft Public License (Ms-PL). You should
  * have received a copy of the license along with the source code. If not, an online copy
